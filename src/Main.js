d41ffa105c7e77ff6d92782b71fb26a163fc7ada
Daniel Leavitt <danleavitt0@gmail.com>
2015-07-16 21:02:25 +0000

    newest

--- src/Main.js
+++ src/Main.js
@@ -2,13 +2,6 @@ var React = require('react')
 var ThemeManager = require('material-ui/lib/styles/theme-manager')()
 import {Avatar, Card, CardHeader, CardText, CardMedia, CardTitle} from 'material-ui'
 
-var styles = {
-  card: {
-    width: 350,
-    margin: 15
-  }
-}
-
 var App = React.createClass({
 
   childContextTypes: {
@@ -24,16 +17,7 @@ var App = React.createClass({
   render: function () {
     return (
       <div>
-        <Card style={styles.card}>
-          <CardHeader title="Daniel" subtitle="Today at 1:07pm" avatar={<Avatar>D</Avatar>} />
-          <CardMedia>
-            <img src="" />
-          </CardMedia>
-          <CardTitle title="Wooooo" subtitle="exciting" />
-          <CardText>
-            This is a lot of text
-          </CardText>
-        </Card>
+
       </div>
     )
   }
