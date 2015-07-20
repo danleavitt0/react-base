3d481ea16070183252a46ea301a8264bc0524c15
Daniel Leavitt <danleavitt0@gmail.com>
2015-07-20 20:12:59 +0000

    gabriela fix

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,25 @@
 var React = require('react')
 var ThemeManager = require('material-ui/lib/styles/theme-manager')()
-import {Avatar, Card, CardHeader, CardText, CardMedia, CardTitle} from 'material-ui'
+import {AppBar, Avatar, Card, CardHeader, CardText, CardMedia, CardTitle} from 'material-ui'
+import {ColumnLayout} from 'react-components-9dots'
+import Posts from '../utils/Posts'
+
+var styles = {
+  card: {
+    width: 350,
+    margin: 20,
+    backgroundColor: 'lemonchiffon'
+  },
+  header: {
+    backgroundColor: 'lightblue'
+  },
+  container: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    flexWrap: 'wrap'
+  }
+}
 
 var App = React.createClass({
 
@@ -15,9 +34,26 @@ var App = React.createClass({
   },
 
   render: function () {
+    var posts = Posts.map(function(post){
+      return (
+        <Card style={styles.card}>
+          <div>
+            <CardHeader title={post.author} subtitle={post.time} avatar={<Avatar>{post.author[0]}</Avatar>} />
+            <CardMedia>
+              <img src={post.image} />
+            </CardMedia>
+            <CardTitle title={post.title} subtitle={post.subtitle} />
+            <CardText>
+              {post.content}
+            </CardText>
+          </div>
+        </Card>
+      )
+    })
     return (
-      <div>
-
+      <div style={styles.container}>
+        <AppBar title='example' />
+        <ColumnLayout cards={posts}/>
       </div>
     )
   }
