e970c48b2337c10fe041a271beb147d16b253cfe
Daniel Leavitt <danleavitt0@gmail.com>
2015-07-21 19:51:35 +0000

    update to yesterday

---
+++ utils/Posts.js
@@ -0,0 +1,20 @@
+var posts = [
+    {
+        author: 'Super Fun 9 Dots Time',
+        time: 'Today at 2:36',
+        image: 'http://www.magnificentrevolution.org/wp-content/uploads/2014/05/pixar.jpeg',
+        title: 'Cool Beans',
+        subtitle: 'Team Cell Phone',
+        content: 'This is a great sentence about team cell phone and cool beans.'
+    },
+    {
+        author: 'Ime',
+        time: 'Today at 2:36',
+        image: 'http://www.magnificentrevolution.org/wp-content/uploads/2014/05/pixar.jpeg',
+        title: 'Cool Beans',
+        subtitle: 'Team Cell Phone',
+        content: 'This is a great sentence about team cell phone and cool beans.'
+    }
+]
+
+module.exports = posts
